3a3edc33d360a5d4ffe34eebc062db0de6000753
郭振涛 <gztcode@gmail.com>
2022-03-18 14:41:15 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -39,7 +39,7 @@ jsproxy_config({
       hidden: true,
       lines: {
         // 收费版（高权重）
-        'node-cfworker-2.etherdream.com': 4,
+        'a.gzt.workers.dev': 4,
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
@@ -55,7 +55,7 @@ jsproxy_config({
    * 默认节点
    */
   // node_default: 'mysite',
-  node_default: /github\.io$/.test(location.host) ? 'demo-hk' : 'demo-cf1',
+  node_default: /github\.io$/.test(location.host) ? 'cfworker' : 'cfworker',
 
   /**
    * 加速节点
