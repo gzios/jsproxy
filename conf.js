920fb3a5a50994ac0ab6e9e5a3bb73e0766c7981
郭振涛 <gztcode@gmail.com>
2022-04-18 11:38:32 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -41,12 +41,10 @@ jsproxy_config({
     },
     // 该节点用于加载大体积的静态资源
     'cfworker': {
-      label: '',
-      hidden: true,
+      label: '自定义站点',
+      
       lines: {
-        // 收费版（高权重）
-        'a.gzt.workers.dev': 4,
-
+        
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
         'b.007.workers.dev': 1,
